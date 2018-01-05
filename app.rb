189bddbde72acd48bed22d94eb3f2ecfe301bd56
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-05 22:53:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -44,6 +44,8 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
     @opt = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
     if @opt == "add"
       "#{@num1 + @num2}"
     elsif @opt == "subtract"
