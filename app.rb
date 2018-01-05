cec0c40be63ac46887301f62f453b6cdb8fd037b
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-05 22:47:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class App < Sinatra::Base
     @word2 = params[:word2] + " "
     @word3 = params[:word3] + " "
     @word4 = params[:word4] + " "
-    @word5 = params[:word5]
+    @word5 = params[:word5] + "."
     @phrase = @word1 + @word2 + @word3 + @word4 + @word5
     "#{@phrase}"
   end
