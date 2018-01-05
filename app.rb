1fea4d6070c910449b4a6823fd3eb267522bf61c
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-05 22:04:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @name = params[:name]
+    r_name = ""
+    @name.each do |a|
+      r_name = r_name + a 
+    end
+    "#{r_name}"
+    
+  end
 
 end
