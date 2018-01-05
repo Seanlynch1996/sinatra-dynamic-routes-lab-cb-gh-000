35f47093e37e4f1d1c5a9a498d318cf9d6c5c372
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-05 22:39:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     result = ""
-    while i < fin
+    while i < @num
       result += @phrase
       i += 1
     end
